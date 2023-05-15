05b998e36f1eb643278b1c5480e75a6e20826d24
sunhuadong01 <sunhuadong01@zhihu.com>
2023-05-15 15:52:25 +0800

    fix: fixed bug when send elasticemail

--- apps/chat/src/lib/email/elasticemail.ts
+++ apps/chat/src/lib/email/elasticemail.ts
@@ -13,16 +13,33 @@ export default async function sendEmail(to: string, code: string | number) {
     bodyHtml: `您的激活码是：${code}`,
   };
 
-  const formData = new FormData();
-  for (const key in params) {
-    // @ts-ignore
-    formData.append(key, params[key]);
+  const queryString: string = new URLSearchParams(params).toString();
+
+  try {
+    const response = await fetch(`https://api.elasticemail.com/v2/email/send?${queryString}`, {
+      method: "GET",
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    return true;
+  } catch (error) {
+    console.error('There was a problem with the fetch operation: ', error);
+    return false;
   }
 
-  const response = await fetch("https://api.elasticemail.com/v2/email/send", {
-    method: "POST",
-    body: formData,
-  });
+  // const formData = new FormData();
+  // for (const key in params) {
+  //   // @ts-ignore
+  //   formData.append(key, params[key]);
+  // }
 
-  return response.ok;
+  // const response = await fetch("https://api.elasticemail.com/v2/email/send", {
+  //   method: "POST",
+  //   body: formData,
+  // });
+  // return response.ok;
+
 }
